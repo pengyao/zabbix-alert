051cafc42a65087c325533db7e2fee105b523d45
pengyao <pengyao@pengyao.org>
2014-07-03 16:17:58 +0800

    Add Twilio Call Notifier

---
+++ twilio_call.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding=utf8
+
+import os
+from zabbixalert.notifier.twilio_call import TwilioCallNotifier
+from zabbixalert.exceptions import NotifyError
+from zabbixalert.utils import notify
+
+__config_section__ = 'twilio'
+__requisit_opts__ = ['from', 'sid', 'token']
+notifier = TwilioCallNotifier
+
+if __name__ == '__main__':
+    config_file = 'config.yaml'
+    config_file = os.path.dirname(os.path.realpath(__file__)) + os.sep + config_file
+    ret = notify(config_file, __config_section__, __requisit_opts__, notifier)
+    if not ret['result']:
+       raise NotifyError(';'.join(ret['comment']))
+    else:
+        print ';'.join(ret['comment'])
---
+++ zabbixalert/notifier/twilio_call.py
@@ -0,0 +1,53 @@
+__author__ = 'pengyao'
+
+from urllib import urlencode
+from zabbixalert.notifier import BaseNotifier
+from twilio.rest import TwilioRestClient
+from twilio import twiml
+
+class TwilioCallNotifier(BaseNotifier):
+    '''
+    Twilio Call Notifier
+    '''
+    def _mk_twiml(self, message):
+        '''
+        Make Twiml Say XML
+        '''
+        call = self.options.get('call', {})
+        voice = call.get('voice', 'alice')
+        language = call.get('language', 'en-US')
+        loop = call.get('loop', 1)
+        say = twiml.Say(message, voice=voice, language=language, loop=loop)
+        return say.toxml()
+
+    def notify(self, to, subject, message):
+        '''
+        Twilio Call notify
+        '''
+        ret = {
+            "result": True,
+            "comment": [],  
+        }
+        token = self.options['token']
+        sid = self.options['sid']
+        from_ = self.options['from']
+        if not to.startswith('+'):
+            to = '+' + to
+        client = TwilioRestClient(sid, token)
+        twiml_xml = self._mk_twiml(subject.decode('utf-8'))
+        twiml_echo = urlencode({'Twiml': twiml_xml})
+        twiml_url = 'http://twimlets.com/echo?' + twiml_echo
+        method = self.options.get('method', 'POST')
+        
+        try:
+            client.calls.create(
+                url=twiml_url,
+                method=method,
+                from_=from_,
+                to=to)
+        except Exception, e:
+            ret['result'] = False
+            ret['comment'].append(str(e))
+            return ret
+        ret['comment'].append('send call success!')
+        return ret
